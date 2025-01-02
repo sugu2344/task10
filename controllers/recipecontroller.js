95c206f3ee80e7609b4b4369f54c7f78672ee940
sugu2344 <suganesh234@gmail.com>
2025-01-02 15:18:26 +0530

    step6:create recipe controller done

--- controllers/recipecontroller.js
+++ controllers/recipecontroller.js
@@ -1,8 +1,22 @@
 const recipe = require("../models/recipemodel");
 const recipeController = {
   // createRecipe
-  createRecipe: (request, response) => {
-    response.json({ message: "recipe created sucessfully" });
+  createRecipe: async (request, response) => {
+    try {
+      const { title, chefName, createdAt, instructions, ingredients } =
+        request.body;
+      const newRecipe = new recipe({
+        title,
+        chefName,
+        createdAt,
+        instructions,
+        ingredients,
+      });
+      await newRecipe.save();
+      response.json({ message: "recipe created sucessfully" });
+    } catch {
+      response.status(500).json({ message: error.message });
+    }
   },
   // getAllRecipes GET
   getAllRecipes: (request, response) => {
