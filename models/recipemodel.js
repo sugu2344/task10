88a0d6e72628a8dc76561127e3ad43333bd515fc
sugu2344 <suganesh234@gmail.com>
2025-01-02 11:21:34 +0530

    step3: model adde

---
+++ models/recipemodel.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+
+const recipeSchema = new mongoose.Schema({
+  title: String,
+  chefName: String,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    },
+  instructions:String,
+  ingredients:[String]
+});
+
+module.exports = mongoose.model("recipe", recipeSchema,"recipes");
